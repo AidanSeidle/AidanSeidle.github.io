b618b41000b5b971326bba1e808ff59c533032e1
Aidan Seidle <46039134+AidanSeidle@users.noreply.github.com>
2025-01-23 02:11:20 -0500

    Reskin of C Mode

--- yerba_rain.js
+++ yerba_rain.js
@@ -1,46 +1,46 @@
-function activateYerba() {
-    var $yerbas = $();
-    var qt = 10; // Number of yerbas to generate
+function generateYerba() {
+    // Create a random yerba element
+    var $enlightenmint = $('<img class="yerba" src="/media/Enlightenmint.png">');
+    var $berrylemonade = $('<img class="yerba" src="/media/BerryLemonade.png">');
+    var $looseleaf = $('<img class="yerba" src="/media/LooseLeaf.png">');
+    var $unsweetglass = $('<img class="yerba" src="/media/UnsweetGlass.png">');
 
-    for (var i = 0; i < qt; ++i) {
-        var $enlightenmint = $('<img class="yerba" src="/media/Enlightenmint.png">');
-        var $berrylemonade = $('<img class="yerba" src="/media/BerryLemonade.png">');
-        var $looseleaf = $('<img class="yerba" src="/media/LooseLeaf.png">');
-        var $unsweetglass = $('<img class="yerba" src="/media/UnsweetGlass.png">');
+    var $roll = Math.floor(Math.random() * 10); // Random number between 0-9
+    var $currentyerba;
 
-        var $roll = Math.floor(Math.random() * 10); // Random number between 0-9
-
-        var $currentyerba = $(); // Initialize empty jQuery object
-
-        if ($roll === 9) { // Adjusted for valid range (0-9)
-            $currentyerba = $looseleaf;
-        } else if ($roll === 8) {
-            $currentyerba = $unsweetglass;
-        } else if ($roll % 2 === 0) {
-            $currentyerba = $enlightenmint;
-        } else {
-            $currentyerba = $berrylemonade;
-        }
-
-        $currentyerba.css({
-            'left': Math.random() * $('#site').width() + 'px',
-            'top': -Math.random() * $('#site').height() + 'px'
-        });
-
-        $yerbas = $yerbas.add($currentyerba); // Add the current yerba to the collection
+    if ($roll === 9) {
+        $currentyerba = $looseleaf;
+    } else if ($roll === 8) {
+        $currentyerba = $unsweetglass;
+    } else if ($roll % 2 === 0) {
+        $currentyerba = $enlightenmint;
+    } else {
+        $currentyerba = $berrylemonade;
     }
 
-    $('#yerba').prepend($yerbas); // Add all yerbas to the DOM at once
+    // Set random initial position above the container
+    $currentyerba.css({
+        left: Math.random() * $('main').width() + 'px',
+        top: -Math.random() * $('main').height() + 'px',
+        transform: 'rotate(' + (Math.random() * 90 - 45) + 'deg)' // Rotate between -45 to 45 degrees
+    });
 
-    $yerbas.animate({
-        top: $(window).height(), // Animate falling
-    }, Math.random() * 2000 + 5000, function () { // Random animation duration between 5-7 seconds
-        $(this).remove(); // Remove the element after animation
+    $('#yerba').prepend($currentyerba); // Add the yerba to the DOM
+    animateYerba($currentyerba); // Animate the newly created yerba
+}
 
-        // Re-activate yerba if all have fallen
-        if (--qt < 1) {
-            activateYerba();
-        }
+function animateYerba($yerba) {   
+    $yerba.animate({
+        top: $('main').height() - $($yerba).height(), // Stop above the footer based on its position
+    }, Math.random() * 2000 + 8000, "swing", function () {
+        $(this).remove(); // Remove the yerba after it reaches the bottom
+        generateYerba(); // Generate a new yerba immediately
     });
 }
-activateYerba();
+
+// Start the initial trickle of yerbas
+for (var i = 0; i < 15; ++i) {
+    setTimeout(function() {
+        generateYerba();
+    }, Math.random() * 5000)
+}
